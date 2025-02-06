3af1feeb93e100078622d2d96d8872b8eca1d960
BilyAlv <bilymanuelm000@gmail.com>
2025-02-05 20:10:34 -0400

    Commit Changes

--- test_hello.py
+++ test_hello.py
@@ -3,7 +3,7 @@ from hello import hello_world
 
 class TestHelloWorld(unittest.TestCase):
     def test_hello(self):
-        self.assertEqual(hello_world(), "Hola mundo!")
+        self.assertEqual(hello_world(), "Hola Mundo!")
 
 if __name__ == "__main__":
     unittest.main()
